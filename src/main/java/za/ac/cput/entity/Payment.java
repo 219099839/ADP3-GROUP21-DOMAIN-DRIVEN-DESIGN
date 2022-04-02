6a1b6209176de583fad39e9f1f8716afec204a5e
Zelino Pestana-218325991 <zelinopestana@gmail.com>
2022-04-02 20:15:35 +0200

    218325991 - Entities - 1st commit

---
+++ src/main/java/za/ac/cput/entity/Payment.java
@@ -0,0 +1,77 @@
+package za.ac.cput.entity;
+
+/* Zelino Pestana - 218325991
+ *
+ */
+
+public class Payment {
+    private String paymentID;
+    private double orderAmount;
+    private String paymentOption;
+
+    private Payment() {
+    }
+
+    private Payment(Builder builder) {
+        this.paymentID = builder.paymentID;
+        this.orderAmount = builder.orderAmount;
+        this.paymentOption = builder.paymentOption;
+    }
+
+    public String getPaymentID() {
+        return paymentID;
+    }
+    public double getOrderAmount() {
+        return orderAmount;
+    }
+    public String getPaymentOption() {
+        return paymentOption;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "paymentID='" + paymentID + '\'' +
+                ", orderAmount=" + orderAmount + '\'' +
+                ", paymentOption='" + paymentOption +
+                '}';
+    }
+
+    public static class Builder {
+
+        private String paymentID;
+        private double orderAmount;
+        private String paymentOption;
+
+        public Builder setPaymentOption(String paymentOption) {
+            this.paymentOption = paymentOption;
+            return this;
+        }
+
+        public Builder setPaymentID(String paymentID) {
+            this.paymentID = paymentID;
+            return this;
+        }
+
+        public Builder setOrderAmount(double orderAmount) {
+            this.orderAmount = orderAmount;
+            return this;
+        }
+
+        public Builder copy(Payment payment) {
+            this.paymentOption = payment.paymentOption;
+            this.orderAmount = payment.orderAmount;
+            this.paymentID = payment.paymentID;
+            return this;
+        }
+
+        public Payment build() {
+
+            return new Payment(this);
+
+        }
+
+    }
+
+}
+
---
+++ src/main/java/za/ac/cput/entity/Receipt.java
@@ -0,0 +1,80 @@
+package za.ac.cput.entity;
+
+/* Zelino Pestana 218325991
+ *
+ */
+
+public class Receipt {
+
+    public String receiptID;
+    public String date;
+    private double amount;
+
+    private Receipt() {
+    }
+
+    private Receipt(Builder builder) {
+        this.date = builder.date;
+        this.receiptID = builder.receiptID;
+        this.amount = builder.amount;
+
+    }
+
+    public String getReceiptID() {
+        return receiptID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Receipt{" +
+                "receiptID='" + receiptID + '\'' +
+                ", date='" + date + '\'' +
+                ", amount='" + amount +
+                '}';
+    }
+
+    public static class Builder {
+
+        public String receiptID;
+        public String date;
+        private double amount;
+
+
+        public Builder setReceiptID(String receiptID) {
+            this.receiptID = receiptID;
+            return this;
+        }
+
+        public Builder setDate(String date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder setAmount(double amount) {
+            this.amount = amount;
+            return this;
+        }
+
+
+        public Builder copy(Receipt receipt) {
+            this.date = receipt.date;
+            this.receiptID = receipt.receiptID;
+            this.amount = receipt.amount;
+            return this;
+        }
+
+        public Receipt build() {
+
+            return new Receipt(this);
+        }
+    }
+}
+
